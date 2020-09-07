71eb9208f4ef8a5538d54fdece2dd12ffe2d282c
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-07 23:27:09 +0200

    Add savegame

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [UnityEditor.MenuItem("Dev Tools/Delete Save")]
+    public static void DeleteSaves()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
     [SerializeField]
     private List<string> collectedKeys = new List<string>();
     [SerializeField]
     private int lives;
+
+    private void Awake()
+    {
+        Load();
+    }
 
     public void RemoveLife()
     {
@@ -31,5 +42,27 @@ public class Game : MonoBehaviour
     public bool HasKey(string key)
     {
         return collectedKeys.Contains(key);
+    }
+
+    private void OnDestroy()
+    {
+        Save();
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("KeyCount", collectedKeys.Count);
+        for (int i = 0; i < collectedKeys.Count; i++)
+        {
+            PlayerPrefs.SetString("key" + i, collectedKeys[i]);
+        }
+    }
+
+    private void Load()
+    {
+        for (int i = 0; i < PlayerPrefs.GetInt("KeyCount"); i++)
+        {
+            collectedKeys.Add(PlayerPrefs.GetString("key"+i));
+        }
     }
 }
