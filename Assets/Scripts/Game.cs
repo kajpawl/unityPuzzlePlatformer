f5d0e2f18616424750a3a33d39e14b6f556d7789
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-08 00:27:17 +0200

    Update UI and update save function

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [SerializeField]
+    private List<string> collectedKeys = new List<string>();
+    [SerializeField]
+    private int lives;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI[] keyCountTexts;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI livesText;
+
     [UnityEditor.MenuItem("Dev Tools/Delete Save")]
     public static void DeleteSaves()
     {
         PlayerPrefs.DeleteAll();
     }
-
-    [SerializeField]
-    private List<string> collectedKeys = new List<string>();
-    [SerializeField]
-    private int lives;
 
     private void Awake()
     {
@@ -27,16 +32,19 @@ public class Game : MonoBehaviour
         {
             Debug.Log("Game over");
         }
+        UpdateUI();
     }
 
     public void AddKey(string key)
     {
         collectedKeys.Add(key);
+        UpdateUI();
     }
 
     public void RemoveKey(string key)
     {
         collectedKeys.Remove(key);
+        UpdateUI();
     }
 
     public bool HasKey(string key)
@@ -49,9 +57,18 @@ public class Game : MonoBehaviour
         Save();
     }
 
+    private void UpdateUI()
+    {
+        keyCountTexts[0].text = collectedKeys.Count(key => key == "green").ToString();
+        keyCountTexts[1].text = collectedKeys.Count(key => key == "red").ToString();
+        keyCountTexts[2].text = collectedKeys.Count(key => key == "blue").ToString();
+        livesText.text = lives.ToString();
+    }
+
     private void Save()
     {
         PlayerPrefs.SetInt("KeyCount", collectedKeys.Count);
+        PlayerPrefs.SetInt("Lives", lives);
         for (int i = 0; i < collectedKeys.Count; i++)
         {
             PlayerPrefs.SetString("key" + i, collectedKeys[i]);
@@ -60,9 +77,11 @@ public class Game : MonoBehaviour
 
     private void Load()
     {
+        lives = PlayerPrefs.GetInt("Lives", 3);
         for (int i = 0; i < PlayerPrefs.GetInt("KeyCount"); i++)
         {
             collectedKeys.Add(PlayerPrefs.GetString("key"+i));
         }
+        UpdateUI();
     }
 }
